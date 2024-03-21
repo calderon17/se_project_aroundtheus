fb090dfed90a1d01b20505c58e907155bccafbdb
Manuel Calderon <manuu.calderonn@gmail.com>
2024-03-20 23:13:44 -0400

    project 6, completed, 1st try

--- scripts/index.js
+++ scripts/index.js
@@ -61,30 +61,22 @@ const preImgModalCloseButton = previewImageModal.querySelector("#modal-close");
 //----------------------------------------------------------------------------------------
 //                                     Functions
 //----------------------------------------------------------------------------------------
-/////////////////////////////
 function handleEsc(evt) {
   if (evt.key === "Escape") {
-    document.querySelector(".modal_opened");
+    const handleEscUp = document.querySelector(".modal_opened");
     closePopup(handleEscUp);
   }
 }
 
-const handleEscUp = (evt) => {
-  evt.preventDefault();
-  isEscEvent(evt, closePopup);
-};
-
 function closePopup(popup) {
   popup.classList.remove("modal_opened");
-  document.addEventListener("keydown", handleEscUp);
+  document.removeEventListener("keydown", handleEsc);
 }
 
 function openPopup(popup) {
   popup.classList.add("modal_opened");
-  document.removeEventListener("keydown", handleEscUp);
+  document.addEventListener("keydown", handleEsc);
 }
-
-/////////////////////
 
 function getCardElement(cardData) {
   const cardElement = cardTemplate.cloneNode(true);
