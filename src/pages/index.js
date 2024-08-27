42d75f2ffafa6dad13b9eff53018734b1fe95c3a
Manuel Calderon <manuu.calderonn@gmail.com>
2024-08-27 04:18:58 -0400

    project 8, completed, discord help with section only add cards

--- src/pages/index.js
+++ src/pages/index.js
@@ -40,82 +40,21 @@ import {
 
 console.log(initialCards);
 console.log(profileEditButton);
-// editFormValidator.enableValidation();
-// addFormValidator.enableValidation();
-
-// const initialCards = [
-//   {
-//     name: "Yosemite Valley",
-//     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
-//   },
-//   {
-//     name: "Lake Louise",
-//     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
-//   },
-//   {
-//     name: "Bald Mountains",
-//     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
-//   },
-//   {
-//     name: "Latemar",
-//     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
-//   },
-//   {
-//     name: "Vanoise National Park",
-//     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
-//   },
-//   {
-//     name: "Lago di Braies",
-//     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
-//   },
-// ];
-
-// //----------------------------------------------------------------------------------------
-// //                                      Elements
-// //----------------------------------------------------------------------------------------
-// const profileEditButton = document.querySelector(".profile__edit-button");
-// const profileEditModal = document.querySelector("#profile-edit-modal");
-// const addCardModal = document.querySelector("#add-card-modal");
-// const profileModalCloseButton = profileEditModal.querySelector("#modal-close");
-// const addCardModalCloseButton = addCardModal.querySelector("#modal-close");
-// const profileTitle = document.querySelector(".profile__title");
-// const profileDescription = document.querySelector(".profile__description");
-// const profileTitleInput = document.querySelector("#profile-title-name");
-// const profileDescriptionInput = document.querySelector(
-//   "#profile-description-input"
-// );
-
-// const addNewCardButton = document.querySelector(".profile__add-button");
-
-// const profileEditForm = profileEditModal.querySelector("#modal-form");
-// const addCardFormElement = addCardModal.querySelector("#modal-form");
-// const cardTitleInput = addCardFormElement.querySelector("#card-title-input");
-// const cardUrlInput = addCardFormElement.querySelector("#card-url-input");
-
-// const cardListEl = document.querySelector(".cards__list");
-// const cardImageEL = document.querySelector(".cards__list");
-// const Template =
-//   document.querySelector("#card-template").content.firstElementChild;
-
-// const previewImageModal = document.querySelector("#preview-image-modal");
-// const modalImageElement = previewImageModal.querySelector(".modal__img");
-// const imageModalcaption = previewImageModal.querySelector(".modal__caption");
-// const preImgModalCloseButton = previewImageModal.querySelector("#modal-close");
-
 //________________sprint 8
 
 // instances
 
+// create a card using the Card class
+// render the card using cardSection.addItem(<pass-in-element-here>)
+// cardSection.renderCard(cardData); didnt work
+// call the render card function that has both
+// cardSection.addItem(cardData);
+// createCard.inputValues();
+
 const cardSection = new Section(
   {
-    items: initialCards,
+    item: initialCards.forEach(renderCard),
     renderer: () => {
-      // create a card using the Card class
-      // render the card using cardSection.addItem(<pass-in-element-here>)
-      // cardSection.renderCard(cardData); didnt work
-      // call the render card function that has both
-      // cardSection.addItem(cardData);
-      // createCard.inputValues();
       cardSection.addItem();
     },
   },
@@ -163,60 +102,6 @@ function renderCard(item) {
   cardListEl.prepend(cardElement);
 }
 
-// function renderCard(item) {
-//   const cardElement = createCard(item);
-//   cardListEl.prepend(cardElement);
-// }
-
-// function handleEsc(evt) {
-//   if (evt.key === "Escape") {
-//     const openedModal = document.querySelector(".modal_opened");
-//     closePopup(openedModal);
-//   }
-// }
-
-// function closePopup(popup) {
-//   popup.classList.remove("modal_opened");
-//   document.removeEventListener("keydown", handleEsc);
-// }
-
-// function openPopup(popup) {
-//   popup.classList.add("modal_opened");
-//   document.addEventListener("keydown", handleEsc);
-// }
-
-// Close on overlay
-
-// function addCloseListener(modalElement) {
-//   modalElement.addEventListener("mousedown", (evt) => {
-//     if (
-//       evt.target.classList.contains("modal") ||
-//       evt.target.classList.contains("modal__close")
-//     ) {
-//       closePopup(modalElement);
-//     }
-//   });
-// }
-
-// addCloseListener(profileEditModal);
-// addCloseListener(addCardModal);
-// addCloseListener(previewImageModal);
-
-//----------------------------------------------------------------------------------------
-//                                     Validation
-//----------------------------------------------------------------------------------------
-///validation
-
-// const cardSelector = "#card-template";
-
-// const settings = {
-//   inputSelector: ".modal__input",
-//   submitButtonSelector: ".modal__button",
-//   inactiveButtonClass: "modal__button_disabled",
-//   inputErrorClass: "modal__input_type_error",
-//   errorClass: "modal__error_visible",
-// };
-
 const editFormValidator = new FormValidator(settings, profileEditForm);
 const addFormValidator = new FormValidator(settings, addCardFormElement);
 
@@ -263,7 +148,7 @@ profileEditButton.addEventListener("click", () => {
 
 // profileEditForm.addEventListener("submit", editProfilePopup);
 
-initialCards.forEach(renderCard);
+// initialCards.forEach(renderCard);
 
 // Add new card
 
